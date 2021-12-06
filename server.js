7dee2fcfa12a6935c9336d6ef88788e33717b52a
Eduard Marcencov <90940827+eddiem22@users.noreply.github.com>
2021-12-05 23:51:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,16 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
 app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
+
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
+
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome!" });
 });
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);

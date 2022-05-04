4e94e2db671f3cf489f3b569c19cffa8903611f4
Eduard Marcencov <90940827+eddiem22@users.noreply.github.com>
2022-05-04 19:02:50 -0400

    Update websiteModules.js

--- Desktop/node-sequelize/server/controllerModules/websiteModules.js
+++ Desktop/node-sequelize/server/controllerModules/websiteModules.js
@@ -1,20 +1,21 @@
 const Website = require("../models").Website;
 const generateHash = require('../utils/hashManager').hasher;
 var randomRSAKeyGenerator = require("../utils/randomRSAKeyGenerator").getRandomPrime;
+var askSecurityScript = require("../utils/askSecurityScript").securityCommand;
 const fetch = require("isomorphic-fetch");
+const { securityCommand } = require("../utils/askSecurityScript");
 module.exports = {
-  
 
     async updateWebsite(websiteToBeUpdated, req) {
         try{
-          
+	if(typeof(req.query.URL) !== undefined) {
           if(websiteToBeUpdated){
             await Website.update(
               {
-                  securityFlag: req.body.securityFlag ? req.securityFlag : await fetch(`http://192.168.1.195:3456/api/website/check?URL=${URL}&command=-d%20-j`).then((result) =>{if(result['missing'].length> 5) {return false} else{return true}}),
+                  securityFlag: (!websiteToBeUpdated.securityFlag && !req.body.securityFlag) ? false : (!websiteToBeUpdated.securityFlag && req.body.securityFlag) ? req.body.securityFlag : (websiteToBeUpdated.securityFlag && !req.body.securityFlag) ? websiteToBeUpdated.securityFlag : false,
                   categoryID: (!websiteToBeUpdated.categoryID && !req.body.categoryID) ? 1 : (!websiteToBeUpdated.categoryID && req.body.categoryID) ? req.body.categoryID : (websiteToBeUpdated.categoryID && !req.body.categoryID) ? websiteToBeUpdated.categoryID : 1,
                   RSA_Key: (!websiteToBeUpdated.RSA_Key && !req.body.RSA_Key) ? await randomRSAKeyGenerator([100,1000]) : (!websiteToBeUpdated.RSA_Key && req.body.RSA_Key) ? req.body.RSA_Key : (websiteToBeUpdated.RSA_Key && !req.body.RSA_Key) ? websiteToBeUpdated.RSA_Key : await randomRSAKeyGenerator([100,1000]),
-                  fromwhitelist: (!websiteToBeUpdated.fromwhitelist && !req.body.fromwhitelist) ? 0 : (!websiteToBeUpdated.fromwhitelist && req.body.fromwhitelist) ? req.body.fromwhitelist : (websiteToBeUpdated.fromwhitelist && !req.body.fromwhitelist) ? websiteToBeUpdated.fromwhitelist : false,
+                  fromwhitelist: (!websiteToBeUpdated.fromwhitelist && !req.body.fromwhitelist) ? false : (!websiteToBeUpdated.fromwhitelist && req.body.fromwhitelist) ? req.body.fromwhitelist : (websiteToBeUpdated.fromwhitelist && !req.body.fromwhitelist) ? websiteToBeUpdated.fromwhitelist : false,
                   hash: (!websiteToBeUpdated.hash && !req.body.hash) ? await generateHash(websiteToBeUpdated.URL)  : (!websiteToBeUpdated.hash && req.body.hash) ? req.body.hash : (websiteToBeUpdated.hash && !req.body.hash) ? websiteToBeUpdated.hash : await generateHash(websiteToBeUpdated.URL),
                 },
                 {
@@ -24,8 +25,10 @@ module.exports = {
               return newWebsite
             })
               }
-              else{return false}
+              else{return null}
           }
+	else{throw "He cant keep getting away with this!!!"}
+	}
           catch(e){
             console.log(e)
             //return e
@@ -34,14 +37,16 @@ module.exports = {
 
         async createWebsite(req) {
             try {
+		if(typeof(req.body.URL) !== undefined) {
                 await Website.findOne({where:{URL: req.body.URL}}).then(async(duplicate) => {
-                    if(duplicate) //IF DUPLICATE FOUND
-                    {throw "duplicate!"}
+                    if(duplicate) {				 //IF DUPLICATE FOUND
+                    throw "Duplicate!!!!"
+                }	
             //IF NO DUPLICATE
             else{
                 await Website.findOrCreate({where: {
                 URL: req.body.URL,
-                securityFlag: req.body.securityFlag ? req.body.securityFlag:  await fetch(`http://192.168.1.195:3456/api/website/check?URL=${req.body.URL}&command=-d%20-j`).then((result) => {if(result['missing'] >= 5) {return false} else{return true}}),
+                securityFlag: req.body.securityFlag ? req.body.securityFlag: false,
                 categoryID: req.body.categoryID ? req.body.categoryID : 1,
                 RSA_Key: req.body.RSA_Key ? req.body.RSA_Key : await randomRSAKeyGenerator([100,1000]),
                 fromwhitelist: req.body.fromwhitelist ? req.body.fromwhitelist : false,
@@ -61,6 +66,8 @@ module.exports = {
                 })
             }
         })
+}
+	else{throw "He cant keep getting away with this!!! Parse your JSON!!!"}
     }
          catch (e) {
                 console.log(e)
